9022f27f9133396d2564f084a7a5a592fa5b04d7
imran787 <imran.mughal87@gmail.com>
2017-06-09 16:13:03 +0100

    rane

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :items_as_seller, foreign_key: "user_id", class_name: "Item"
-  # has_many :items_as_buyer, foreign_key: "user_id", class_name: "Item"
-
   has_many :sent_requests, foreign_key: 'buyer_id', class_name: 'Request'
   has_many :received_requests, foreign_key: 'seller_id', class_name: 'Request'
-  # has_many :items_as_seller, through: :received_requests, source: :item
+  
+  has_many :items_as_seller, foreign_key: "user_id", class_name: "Item"
   has_many :items_as_buyer, through: :sent_requests, source: :item
 
   validates :email, presence: true, uniqueness: true
--- app/serializers/request_serializer.rb
+++ app/serializers/request_serializer.rb
@@ -1,5 +1,4 @@
 class RequestSerializer < ActiveModel::Serializer
-  attributes :id, :buyer_id, :seller_id, :status, :item_id
+  attributes :id, :buyer_id, :seller_id, :status, :item_id, :item
   has_one :item
-
 end
